1729b9173df30256fefb1ecbf4996fb278e75f95
chenxing <chenxing@qq.com>
2020-05-09 17:40:33 +0800

    1

--- test_mysql.php
+++ test_mysql.php
@@ -1,12 +1,12 @@
 <?php
-
-$con = mysqli_connect("localhost", "root", "", "test");
+$con1 = mysqli_connect("localhost", "root", "", "test");
+$con2 = mysqli_connect("localhost", "root", "", "test");
 if (mysqli_connect_errno()) {
     echo "连接 MySQL 失败: " . mysqli_connect_error();
 }
 
 // 执行查询
-mysqli_query($con, "
+mysqli_query($con1, "
 CREATE TABLE IF NOT EXISTS `vote_record` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(32) NOT NULL,
@@ -16,18 +16,24 @@ CREATE TABLE IF NOT EXISTS `vote_record` (
   PRIMARY KEY (`id`),
   KEY `index_user_id` (`user_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
-") or var_dump(mysqli_error($con));
+") or var_dump(mysqli_error($con1));
 
-mysqli_query($con, "
+mysqli_query($con1, "
 CREATE PROCEDURE IF NOT EXISTS `vote_record`(IN n int)
 BEGIN
     DECLARE i INT DEFAULT 1;
+    DECLARE s FLOAT DEFAULT 0;
     WHILE (i <= n) DO
-        INSERT INTO vote_record (user_id, vote_id, group_id, create_time) VALUES (MD5(RAND()), FLOOR(RAND() * 1000), FLOOR(RAND() * 100), NOW());
+        SET s = RAND();
+        INSERT INTO vote_record VALUES (NULL, MD5(s), FLOOR(s * 1000), FLOOR(s * 100), NOW());
         SET i = i + 1;
     END WHILE;
 END
-") or var_dump(mysqli_error($con));
+") or var_dump(mysqli_error($con1));
 
-mysqli_query($con, "CALL vote_record(1000000)") or var_dump(mysqli_error($con));
-mysqli_close($con);
+mysqli_query($con1, "CALL vote_record(1000000)", MYSQLI_ASYNC);
+mysqli_query($con2, "CALL vote_record(1000000)", MYSQLI_ASYNC);
+$read = $errors = $reject = [$con1, $con2];
+mysqli_poll($read, $errors, $reject, 1) or var_dump(mysqli_error($con1));
+
+mysqli_close($con1);
